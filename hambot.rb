7fa62957c921def101fcbea6e543f3b2f60013bc
ceejaay <chad.jemmett@gmail.com>
2013-02-08 16:30:21 -0700

    made some text changes

--- hambot.rb
+++ hambot.rb
@@ -7,14 +7,14 @@ require 'gmail'
 #the methods and blocks and stuff here are from the gmail gem.
 def hambot
 
-  mail_account = Gmail.connect("your_email@gmail.com","your_password")
+  mail_account = Gmail.connect("hambot9000@gmail.com","borkingbad")
   puts mail_account.logged_in? #returns true if your account on line 7 is logged in.
 
   mail_account.deliver do
-    to "target_email@anymail.com"
+    to "chad.jemmett@gmail.com"
     subject "Hello from Hambot"
     text_part do
-      body "Hello, I am Hambot9000. Please do not worry I follow the three laws of spambots. 1.A spambot may not injure a human being, or by inaction allow a human to be injured. We just want you to buy boner pills. 2.We ignore any orders from humans, except when you want to buy boner pills. 3.A robot must protect its own existence. So, BLOWOUT SALES ON VIAGR@ CIAL1S. MAKE HER FEAR YOU!"
+      body "Hello from the Hambot9000!"
     end #end of text part
 
 
